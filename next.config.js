3a7645d470ba35d5e2f63d852474a706319d61aa
Sekhon0999 <sekhon0999@gmail.com>
2025-08-29 18:06:38 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+  reactStrictMode: true,
+  // IMPORTANT: Do NOT set `output: 'export'` on Vercel for App Router pages.
+  // No basePath or weird rewrites for now.
 };
 
 module.exports = nextConfig;
